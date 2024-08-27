9f3855fe74589baeb58c5785bbb0617526a81114
yoshiki-maruya <yoshiki.maruya@gmail.com>
2024-08-27 23:22:08 +0900

    [impr]  summarize include file

--- src/atcoder/abc/abc319/a/abc319_a.cpp
+++ src/atcoder/abc/abc319/a/abc319_a.cpp
@@ -1,19 +1,6 @@
 #pragma GCC optimize("Ofast")
 
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cstdio>
-#include <functional>
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <numeric>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
